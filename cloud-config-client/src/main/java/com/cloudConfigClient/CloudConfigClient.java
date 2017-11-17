b0773e5349aacbf607fbd6795f28d17867aed705
enhui.chen <enhui.chen@china-hand.com>
2017-11-17 14:46:36 +0800

    readme的修改

---
+++ cloud-config-client/src/main/java/com/cloudConfigClient/CloudConfigClient.java
@@ -0,0 +1,16 @@
+package com.cloudConfigClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by enHui.Chen on 2017/11/17 0017.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudConfigClient {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClient.class, args);
+    }
+}
