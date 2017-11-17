b4ba19dae2acf81523dcde7365ccb813d834c01c
enhui.chen <enhui.chen@china-hand.com>
2017-11-17 15:32:11 +0800

    readme的修改

--- cloud-person/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into person(id,name) values(hibernate_sequence.nextval,'陈');
-insert into person(id,name) values(hibernate_sequence.nextval,'恩');
-insert into person(id,name) values(hibernate_sequence.nextval,'慧');
-insert into person(id,name) values(hibernate_sequence.nextval,'陈恩慧');
