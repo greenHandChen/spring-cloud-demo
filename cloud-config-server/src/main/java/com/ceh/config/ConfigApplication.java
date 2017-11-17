474ea4823bcf55bf1a728c80773f9a8a92a3a333
enhui.chen <enhui.chen@china-hand.com>
2017-11-17 14:10:43 +0800

    readme的修改

--- cloud-config-server/src/main/java/com/ceh/config/ConfigApplication.java
+++ cloud-config-server/src/main/java/com/ceh/config/ConfigApplication.java
@@ -2,6 +2,7 @@ package com.ceh.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -12,7 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 // 开启配置服务: 用于对其他项目的配置文件进行额外的配置
 @EnableConfigServer
 // 开启作为Eureka server的客户端支持
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
