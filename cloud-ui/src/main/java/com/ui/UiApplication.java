18b5c87d98093586b463dcebdfef3d0eefec0f7f
enhui.chen <enhui.chen@china-hand.com>
2017-11-17 15:58:09 +0800

    readme的修改

--- cloud-ui/src/main/java/com/ui/UiApplication.java
+++ cloud-ui/src/main/java/com/ui/UiApplication.java
@@ -3,6 +3,7 @@ package com.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -11,7 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * Created by enHui.Chen on 2017/9/26 0026.
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 // 开启对feign客户端的支持
 @EnableFeignClients
 // 开启对断路器的支持
